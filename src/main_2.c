2f58bb2ff259911a5b4f1eae7cec2aa2ed2089e2
VVBondarenko <vbondarenko3950@yandex.ru>
2016-12-11 02:11:01 +0300

    generalized realization of IDO scheme to arbitrary value of speed

--- src/main_2.c
+++ src/main_2.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 
-double xL = 0., xR = 1., hx, t, Tmax=6, CFL = 0.515, dt, a = -1.;
-#define Nx 256
+double xL = 0., xR = 1., hx, t, Tmax=6, CFL = 0.5, dt, a = -2.;
+#define Nx 128
 
 double 	Uarr[Nx],
         dUarr[Nx];
@@ -77,6 +77,15 @@ void IDO()
 
     for( t = 0.; t < Tmax; t+=dt )
     {
+        i = 0;
+        A = (-0.75/hx*(Uarr[i+1]-Uarr[Nx-1])+0.25*(dUarr[i+1]+4*dUarr[i]+dUarr[Nx-1]))/hx/hx/hx/hx;
+        B = (-0.5/hx*(Uarr[i+1]-2*Uarr[i]+Uarr[Nx-1]) +0.25*(dUarr[i+1]-dUarr[Nx-1]))/hx/hx/hx;
+        C = (1.25/hx*(Uarr[i+1]-Uarr[Nx-1])-0.25*(dUarr[i+1]+8*dUarr[i]+dUarr[Nx-1]))/hx/hx;
+        D = (1./hx*(Uarr[i+1] -2.*Uarr[i]+Uarr[Nx-1]) -0.25*(dUarr[i+1]-dUarr[Nx-1]))/hx;
+        dUarrN[i] = dUarr[i]-a*2.*dt*D + a*a*3.*dt*dt*C - a*a*a*4.*B*dt*dt*dt;
+        UarrN[i]  = Uarr[i] -a*dt*dUarr[i] + a*a*dt*dt*D - a*a*a*dt*dt*dt*C
+                        + a*a*a*a*dt*dt*dt*dt*B;
+
         for (i = 1; i < Nx-1; i++)
         {
             A = (-0.75/hx*(Uarr[i+1]-Uarr[i-1])+0.25*(dUarr[i+1]+4*dUarr[i]+dUarr[i-1]))/hx/hx/hx/hx;
@@ -85,31 +94,33 @@ void IDO()
             D = (1./hx*(Uarr[i+1] -2.*Uarr[i]+Uarr[i-1]) -0.25*(dUarr[i+1]-dUarr[i-1]))/hx;
 
             //ToDo: add possibility to change a to values, that are different from 1
-            dUarrN[i] = dUarr[i]+ 2.*dt*D + 3.*dt*dt*C + 4.*B*dt*dt*dt;
-            UarrN[i]  = Uarr[i] + dt*dUarr[i]+dt*dt*D+dt*dt*dt*C+dt*dt*dt*dt*B;
+            dUarrN[i] = dUarr[i]-a*2.*dt*D + a*a*3.*dt*dt*C - a*a*a*4.*B*dt*dt*dt;
+            UarrN[i]  = Uarr[i] -a*dt*dUarr[i] + a*a*dt*dt*D - a*a*a*dt*dt*dt*C
+                            + a*a*a*a*dt*dt*dt*dt*B;
         }
         //так как рассматриваем случай a<0, то будем считать, что у нас всё движется налево,
         // а значит и нужна периодчиность только с лева. Просчитаем дополнительно окрестность
         // точки i=0
-        i = 0;
-        A = (-0.75/hx*(Uarr[i+1]-Uarr[Nx-1])+0.25*(dUarr[i+1]+4*dUarr[i]+dUarr[Nx-1]))/hx/hx/hx/hx;
-        B = (-0.5/hx*(Uarr[i+1]-2*Uarr[i]+Uarr[Nx-1]) +0.25*(dUarr[i+1]-dUarr[Nx-1]))/hx/hx/hx;
-        C = (1.25/hx*(Uarr[i+1]-Uarr[Nx-1])-0.25*(dUarr[i+1]+8*dUarr[i]+dUarr[Nx-1]))/hx/hx;
-        D = (1./hx*(Uarr[i+1] -2.*Uarr[i]+Uarr[Nx-1]) -0.25*(dUarr[i+1]-dUarr[Nx-1]))/hx;
-        dUarrN[i] = dUarr[i]+ 2.*dt*D + 3.*dt*dt*C + 4.*B*dt*dt*dt;
-        UarrN[i]  = Uarr[i] + dt*dUarr[i]+dt*dt*D+dt*dt*dt*C+dt*dt*dt*dt*B;
 
         i = Nx-1;
         A = (-0.75/hx*(Uarr[0]-Uarr[i-1])+0.25*(dUarr[0]+4*dUarr[i]+dUarr[i-1]))/hx/hx/hx/hx;
         B = (-0.5/hx*(Uarr[0]-2*Uarr[i]+Uarr[i-1]) +0.25*(dUarr[0]-dUarr[i-1]))/hx/hx/hx;
         C = (1.25/hx*(Uarr[0]-Uarr[i-1])-0.25*(dUarr[i+1]+8*dUarr[i]+dUarr[i-1]))/hx/hx;
         D = (1./hx*(Uarr[0] -2.*Uarr[i]+Uarr[i-1]) -0.25*(dUarr[0]-dUarr[i-1]))/hx;
-        dUarrN[i] = dUarr[i]+ 2.*dt*D + 3.*dt*dt*C + 4.*B*dt*dt*dt;
-        UarrN[i]  = Uarr[i] + dt*dUarr[i]+dt*dt*D+dt*dt*dt*C+dt*dt*dt*dt*B;
+        dUarrN[i] = dUarr[i]-a*2.*dt*D + a*a*3.*dt*dt*C - a*a*a*4.*B*dt*dt*dt;
+        UarrN[i]  = Uarr[i] -a*dt*dUarr[i] + a*a*dt*dt*D - a*a*a*dt*dt*dt*C
+                        + a*a*a*a*dt*dt*dt*dt*B;
 
-
-        UarrN[Nx-1] = UarrN[0];
-        dUarrN[Nx-1] = dUarrN[0];
+        if(a<0)
+        {
+            UarrN[Nx-1] = UarrN[0];
+            dUarrN[Nx-1] = dUarrN[0];
+        }
+        else
+        {
+            UarrN[0] = UarrN[Nx-1];
+            dUarrN[0] = dUarrN[Nx-1];
+        }
         for (i = 0; i < Nx; i++)
         {
             Uarr[i]  = UarrN[i];
